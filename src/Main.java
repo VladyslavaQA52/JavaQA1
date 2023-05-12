02d3d6e499b03ce8c9e35aa5f46d9230eb65fae0
VladyslavaQA52 <120270073+VladyslavaQA52@users.noreply.github.com>
2023-05-12 15:57:00 +0300

    Main added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int amount = 14567;
+        int bonus = 20;
+        System.out.println(amount / bonus);
+
+
     }
 }
